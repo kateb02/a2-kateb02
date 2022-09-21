735a5d3f7d940931021aed00a6f9b7d804c66ccc
Glitch (a2-kateb02) <none>
2022-09-21 03:20:13 +0000

    😁🍈 Updated with Glitch

--- server.improved.js
+++ server.improved.js
@@ -6,11 +6,16 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+// const appdata = [
+//   { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
+//   { 'model': 'honda', 'year': 2004, 'mpg': 30 },
+//   { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+// ]
+
+const appdata = [ 
 ]
+
+const randomNames = ["Bill:", "Fred:", "Tom:", "Oprah:", "Christiano Ronaldo:", "Laurie Leshin:", "Richard:", "You:"]
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -38,33 +43,39 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    // console.log( JSON.parse( dataString ) )
+    if (request.url === "/submit"){
+      const postArray = JSON.parse(dataString)
+      //do transformations 
+      //parse it, store in appdata, stringify app data, then send it
+        appdata.push(postArray) //newest post will be at end:
+      
+      const randomInt = Math.floor(Math.random() * 8)
+      //modify newest post by adding random name:
+      appdata[appdata.length-1].yourname = randomNames[randomInt] + appdata[appdata.length-1].yourname
+      
+      console.log(appdata[appdata.length-1].yourname, " <-- new, modified post")
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+      }
+    
+    response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
+    response.write( JSON.stringify( appdata ) )
+    console.log("appData array as of last submit: ", appdata) //
+    response.end() //this sends JSON.stringify(appdata) back
   })
 }
 
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
-
    fs.readFile( filename, function( err, content ) {
-
      // if the error = null, then we've loaded the file successfully
      if( err === null ) {
-
-       // status code: https://httpstatuses.com
        response.writeHeader( 200, { 'Content-Type': type })
        response.end( content )
-
      }else{
-
        // file not found, error code 404
        response.writeHeader( 404 )
        response.end( '404 Error: File Not Found' )
-
      }
    })
 }
